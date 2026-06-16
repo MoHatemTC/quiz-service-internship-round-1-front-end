f74742dd16cfb0985cd315ecf2db3631c194bb82
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2026-06-16 19:45:41 +0300

    convert font to geist to match the theme

--- components/dashboard/DashboardHeader.tsx
+++ components/dashboard/DashboardHeader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function DashboardHeader({ title, description }: { title: string; description: string }) {
   return (
     <div className="w-full text-center lg:text-start">
-      <h1 className="text-h1 text-primary-800">{title}G</h1>
+      <h1 className="text-h1 text-primary-800">{title}</h1>
       <p className="text-body text-foreground-secondary">{description}</p>
     </div>
   );
