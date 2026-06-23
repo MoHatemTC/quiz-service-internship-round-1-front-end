2070a9d8c89acd0c9c24fa62db941f9565e8ab96
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2026-06-23 21:12:54 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col bg-background text-foreground">{children}</body>
+      <body className="min-h-full bg-background text-foreground">{children}</body>
     </html>
   );
 }
