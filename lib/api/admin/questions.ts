00a702b86e83a518af8217d6228a2f9c69b593fc
Mohamed Zaki <mohamedzzaki2000s@gmail.com>
2026-06-25 11:40:56 +0300

    feat: fetching questions data
    admin can now see list of questions and select or unselect a question

---
+++ lib/api/admin/questions.ts
@@ -0,0 +1,6 @@
+import { apiFetch } from '@/lib/api/client';
+import { QuestionDto } from '@/types/question/question';
+
+export async function getQuestions(): Promise<QuestionDto[]> {
+  return apiFetch<QuestionDto[]>('/api/questions', { requireAuth: false });
+}
